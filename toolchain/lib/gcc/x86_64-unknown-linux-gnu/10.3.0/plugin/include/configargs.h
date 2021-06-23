eadb7693aacab27ca573551195c3687f10e9217f
Alberto Hernández <alberto.hernandezhernandez@gruposantander.com>
2021-06-23 11:39:49 +0200

    Added files

---
+++ toolchain/lib/gcc/x86_64-unknown-linux-gnu/10.3.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/Volumes/build/.build/x86_64-unknown-linux-gnu/src/gcc/configure --build=x86_64-build_apple-darwin19.6.0 --host=x86_64-build_apple-darwin19.6.0 --target=x86_64-unknown-linux-gnu --prefix=/Volumes/tools/x86_64-unknown-linux-gnu --exec_prefix=/Volumes/tools/x86_64-unknown-linux-gnu --with-sysroot=/Volumes/tools/x86_64-unknown-linux-gnu/x86_64-unknown-linux-gnu/sysroot --enable-languages=c,c++ --enable-__cxa_atexit --disable-libmudflap --disable-libgomp --disable-libssp --disable-libquadmath --disable-libquadmath-support --disable-libsanitizer --enable-libmpx --with-gmp=/Volumes/build/.build/x86_64-unknown-linux-gnu/buildtools --with-mpfr=/Volumes/build/.build/x86_64-unknown-linux-gnu/buildtools --with-mpc=/Volumes/build/.build/x86_64-unknown-linux-gnu/buildtools --with-isl=/Volumes/build/.build/x86_64-unknown-linux-gnu/buildtools --enable-lto --enable-threads=posix --enable-target-optspace --with-linker-hash-style=both --enable-plugin --enable-gold --disable-nls --disable-multilib --with-local-prefix=/Volumes/tools/x86_64-unknown-linux-gnu/x86_64-unknown-linux-gnu/sysroot --enable-long-long";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch", "x86-64" } };
